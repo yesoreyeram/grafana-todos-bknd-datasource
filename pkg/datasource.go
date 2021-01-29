f6f9bad42d2373d4208803066002b2d850a7972b
Sriram <yesoreyeram@gmail.com>
2021-01-29 18:06:04 +0000

    using fields from Query

--- pkg/datasource.go
+++ pkg/datasource.go
@@ -7,12 +7,12 @@ import (
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/instancemgmt"
-	"github.com/grafana/grafana-plugin-sdk-go/backend/log"
 	"github.com/grafana/grafana-plugin-sdk-go/data"
 )
 
 type queryModel struct {
-	Format string `json:"format"`
+	Constant  float64 `json:"constant"`
+	QueryText string  `json:"queryText"`
 }
 
 type todosBkdnDatasource struct {
@@ -20,7 +20,6 @@ type todosBkdnDatasource struct {
 }
 
 func (td *todosBkdnDatasource) QueryData(ctx context.Context, req *backend.QueryDataRequest) (*backend.QueryDataResponse, error) {
-	log.DefaultLogger.Info("QueryData", "request", req)
 	response := backend.NewQueryDataResponse()
 	for _, q := range req.Queries {
 		res := td.query(ctx, q)
@@ -31,30 +30,22 @@ func (td *todosBkdnDatasource) QueryData(ctx context.Context, req *backend.Query
 
 func (td *todosBkdnDatasource) query(ctx context.Context, query backend.DataQuery) backend.DataResponse {
 	var qm queryModel
-
 	response := backend.DataResponse{}
-
 	response.Error = json.Unmarshal(query.JSON, &qm)
 	if response.Error != nil {
 		return response
 	}
 
-	if qm.Format == "" {
-		log.DefaultLogger.Warn("format is empty. defaulting to time series")
+	var timeslices []time.Time
+	var valueslices []int64
+	for i := 0; i < int(qm.Constant); i++ {
+		timeslices = append(timeslices, query.TimeRange.From)
+		valueslices = append(valueslices, int64(i))
 	}
-
 	frame := data.NewFrame("response")
-
-	frame.Fields = append(frame.Fields,
-		data.NewField("time", nil, []time.Time{query.TimeRange.From, query.TimeRange.To}),
-	)
-
-	frame.Fields = append(frame.Fields,
-		data.NewField("values", nil, []int64{10, 20}),
-	)
-
+	frame.Fields = append(frame.Fields, data.NewField("time", nil, timeslices))
+	frame.Fields = append(frame.Fields, data.NewField(qm.QueryText, nil, valueslices))
 	response.Frames = append(response.Frames, frame)
-
 	return response
 }
 
