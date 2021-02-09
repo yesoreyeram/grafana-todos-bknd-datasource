cba23bacc4c4c0fe34aead3c563078ff3b3c2d3f
Sriram <yesoreyeram@gmail.com>
2021-02-09 16:46:12 +0000

    instance specific metrics

--- pkg/plugin/metrics.go
+++ pkg/plugin/metrics.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/grafana/grafana-plugin-sdk-go/backend"
 	"github.com/grafana/grafana-plugin-sdk-go/backend/resource/httpadapter"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -14,13 +15,80 @@ import (
 
 const appPrefix = "todo"
 
-var promRequestsTotal = prometheus.NewCounter(prometheus.CounterOpts{
-	Name: fmt.Sprintf("%s_requests_total", appPrefix),
-	Help: "The total number of requests",
-})
+var (
+	promRequestsTotal = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: fmt.Sprintf("%s_plugin_requests_total", appPrefix),
+		Help: "The total number of  requests for the plugin",
+	})
+	promMetricsRequestsTotal = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: fmt.Sprintf("%s_plugin_metrics_requests_total", appPrefix),
+		Help: "The total number of metric requests for the plugin",
+	})
+)
 
 func init() {
 	prometheus.MustRegister(promRequestsTotal)
+	prometheus.MustRegister(promMetricsRequestsTotal)
+}
+
+type metricRegistryInstance struct {
+	Registry *prometheus.Registry
+	Metrics  struct {
+		TotalMetricsRequest prometheus.Counter
+		Constant            *prometheus.GaugeVec
+		Random              *prometheus.GaugeVec
+	}
+}
+
+func newMetricRegistryInstance() *metricRegistryInstance {
+	instance := &metricRegistryInstance{}
+	instance.Registry = prometheus.NewRegistry()
+	instance.Metrics.Constant = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: fmt.Sprintf("%s_constant", appPrefix),
+		Help: "Constant coming from Grafana datasource config",
+	}, []string{"instanceName", "instanceId"})
+	instance.Registry.MustRegister(instance.Metrics.Constant)
+	instance.Metrics.Random = prometheus.NewGaugeVec(prometheus.GaugeOpts{
+		Name: fmt.Sprintf("%s_random", appPrefix),
+		Help: "Random",
+	}, []string{"instanceName", "instanceId"})
+	instance.Registry.MustRegister(instance.Metrics.Random)
+	instance.Metrics.TotalMetricsRequest = prometheus.NewCounter(prometheus.CounterOpts{
+		Name: fmt.Sprintf("%s_metrics_requests_total", appPrefix),
+		Help: "The total number of requests",
+	})
+	instance.Registry.MustRegister(instance.Metrics.TotalMetricsRequest)
+	return instance
+}
+
+var registryInstances map[int64]metricRegistryInstance = make(map[int64]metricRegistryInstance)
+
+func getInstanceRegistry(id int64) metricRegistryInstance {
+	if _, ok := registryInstances[id]; ok {
+		return registryInstances[id]
+	}
+	registryInstances[id] = *newMetricRegistryInstance()
+	return registryInstances[id]
+}
+
+func (registry *metricRegistryInstance) collectMetrics(pluginContext backend.PluginContext, config *instanceConfig) {
+	// Global metric
+	promMetricsRequestsTotal.Inc()
+	// Metric 0 - Total Metrics Requests collection
+	registry.Metrics.TotalMetricsRequest.Inc()
+	// Metric 1 - Constant value involve datasource instance config
+	value := float64(config.PromValue)
+	registry.Metrics.Constant.With(prometheus.Labels{
+		"instanceName": pluginContext.DataSourceInstanceSettings.Name,
+		"instanceId":   fmt.Sprint(pluginContext.DataSourceInstanceSettings.ID),
+	}).Set(value)
+	// Metric 2 - Another Random logic involve datasource instance config
+	randomSeed := rand.NewSource(time.Now().UnixNano())
+	value1 := float64(rand.New(randomSeed).Intn(100)) * 0.01
+	registry.Metrics.Random.With(prometheus.Labels{
+		"instanceName": pluginContext.DataSourceInstanceSettings.Name,
+		"instanceId":   fmt.Sprint(pluginContext.DataSourceInstanceSettings.ID),
+	}).Set(value1)
 }
 
 func promMetricsForInstance() http.Handler {
@@ -30,31 +98,9 @@ func promMetricsForInstance() http.Handler {
 		if pluginContext.DataSourceInstanceSettings != nil {
 			config := &instanceConfig{}
 			json.Unmarshal(pluginContext.DataSourceInstanceSettings.JSONData, &config)
-			newMetricsRegistry := prometheus.NewRegistry()
-			// Metrics Starts here
-			// Metric 1 - Random logic involve datasource instance config
-			myConstantMetrics := prometheus.NewGaugeVec(prometheus.GaugeOpts{
-				Name: fmt.Sprintf("%s_constant", appPrefix),
-				Help: "Constant coming from Grafana datasource config",
-			}, []string{"instanceName", "instanceId"})
-			newMetricsRegistry.MustRegister(myConstantMetrics)
-			myConstantMetrics.With(prometheus.Labels{
-				"instanceName": pluginContext.DataSourceInstanceSettings.Name,
-				"instanceId":   fmt.Sprint(pluginContext.DataSourceInstanceSettings.ID),
-			}).Set(float64(config.PromValue))
-			// Metric 2 - Another Random logic involve datasource instance config
-			myRandomtMetrics := prometheus.NewGaugeVec(prometheus.GaugeOpts{
-				Name: fmt.Sprintf("%s_random", appPrefix),
-				Help: "Random",
-			}, []string{"instanceName", "instanceId"})
-			newMetricsRegistry.MustRegister(myRandomtMetrics)
-			randomSeed := rand.NewSource(time.Now().UnixNano())
-			myRandomtMetrics.With(prometheus.Labels{
-				"instanceName": pluginContext.DataSourceInstanceSettings.Name,
-				"instanceId":   fmt.Sprint(pluginContext.DataSourceInstanceSettings.ID),
-			}).Set(float64(rand.New(randomSeed).Intn(100)) * 0.01)
-			// Mertics Ends here
-			han := promhttp.HandlerFor(newMetricsRegistry, promhttp.HandlerOpts{})
+			registry := getInstanceRegistry(int64(pluginContext.DataSourceInstanceSettings.ID))
+			registry.collectMetrics(pluginContext, config)
+			han := promhttp.HandlerFor(registry.Registry, promhttp.HandlerOpts{})
 			han.ServeHTTP(rw, req)
 		} else {
 			han := promhttp.Handler()
